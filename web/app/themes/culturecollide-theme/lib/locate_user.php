a2d1185a624e014931a1ae3582f8ee2c977df071
pandabrand <pandabrand@gmail.com>
2017-07-25 10:15:10 -0400

    cover app in conditional

--- web/app/themes/culturecollide-theme/lib/locate_user.php
+++ web/app/themes/culturecollide-theme/lib/locate_user.php
@@ -1,31 +1,32 @@
 <?php
 
 function enqueue_scripts_styles_init() {
-
-  $args = array(
-    "post_type" => "city",
-    "numberposts" => -1,
-  );
-  $cities = get_posts( $args );
-  $map_info = array( 'cities' => array( ) );
-  foreach($cities as $city) {
-    $city_output = array(
-      'city_id' => $city->ID,
-      'title' => get_the_title($city->ID),
-      'link' => get_the_permalink($city->ID),
-      'location' => get_field('city_location', $city->ID)
+  if( is_post_type_archive( ['city', 'artist'] ) || is_single( ['city', 'artist'] ) || is_tax( 'location_types' ) ):
+    $args = array(
+      "post_type" => "city",
+      "numberposts" => -1,
     );
-    $map_info['cities'][] = $city_output;
-  }
+    $cities = get_posts( $args );
+    $map_info = array( 'cities' => array( ) );
+    foreach($cities as $city) {
+      $city_output = array(
+        'city_id' => $city->ID,
+        'title' => get_the_title($city->ID),
+        'link' => get_the_permalink($city->ID),
+        'location' => get_field('city_location', $city->ID)
+      );
+      $map_info['cities'][] = $city_output;
+    }
 
-	wp_register_script( 'ajax-script', get_stylesheet_directory_uri().'/dist/scripts/locate_user.js', array('jquery'), null, true ); // jQuery will be included automatically
-  wp_localize_script( 'ajax-script', 'map_info', $map_info );
-  wp_enqueue_script('ajax-script');
+  	wp_register_script( 'ajax-script', get_stylesheet_directory_uri().'/dist/scripts/locate_user.js', array('jquery'), null, true ); // jQuery will be included automatically
+    wp_localize_script( 'ajax-script', 'map_info', $map_info );
+    wp_enqueue_script('ajax-script');
 
-	// get_template_directory_uri() . '/js/script.js'; // Inside a parent theme
-	// get_stylesheet_directory_uri() . '/js/script.js'; // Inside a child theme
-	// plugins_url( '/js/script.js', __FILE__ ); // Inside a plugin
-	wp_localize_script( 'ajax-script', 'ajax_object', array( 'ajaxurl' => admin_url( 'admin-ajax.php' ) ) ); // setting ajaxurl
+  	// get_template_directory_uri() . '/js/script.js'; // Inside a parent theme
+  	// get_stylesheet_directory_uri() . '/js/script.js'; // Inside a child theme
+  	// plugins_url( '/js/script.js', __FILE__ ); // Inside a plugin
+  	wp_localize_script( 'ajax-script', 'ajax_object', array( 'ajaxurl' => admin_url( 'admin-ajax.php' ) ) ); // setting ajaxurl
+  endif;
 }
 add_action('init', 'enqueue_scripts_styles_init');
 
